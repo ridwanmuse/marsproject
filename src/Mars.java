816b4a21eda9e598d3531468252283cb7ea9b18f
ridwanmuse <104872709+ridwanmuse@users.noreply.github.com>
2023-02-21 23:20:46 -0600

    Update Mars.java

--- src/Mars.java
+++ src/Mars.java
@@ -1,94 +1,77 @@
 public class Mars {
-
-    public static void main(String[] args) throws InterruptedException {
-        // Mars Adventure Game Part 1
-        // Create a string variable named ColonyName, and set it equal to your desired name
-        String ColonyName = "Cat Colony";
-
-        // Create and set ShipPopulation equal to 300
+    public static void main(String[] args) throws InterruptedException{
+        String ColonyName = "Titan";
         int ShipPopulation = 300;
-
-        // Create and set ShipFood equal to 4000.00
         double ShipFood = 4000.00;
-
-        // Create and set Landing equal to true
         boolean Landing = true;
 
+        // The landing process takes 2 days, calculate how much food is left after landing Equation:
+        // Food = Food - (Pop * 0.75); Use the equation twice
 
+        ShipFood = (ShipFood - 2 *(ShipPopulation * 0.75));
+        // ShipFood = ShipFood - (ShipPopulation * 0.75);
 
-        // Upon reaching Mars everyone will eat 0.75 meals a day, to preserve food
-        // The landing process takes 2 days, calculate how much food is left after landing
-        ShipFood = ShipFood - (ShipPopulation * 0.75);
-        ShipFood = ShipFood - (ShipPopulation * 0.75);
-
+        // Amount of food left
         System.out.println(ShipFood);
 
-        // Food added. Increase by 50%
-        ShipFood = ShipFood + (ShipFood * .5);
-
-        // Five people added to population
-        ShipPopulation = ShipPopulation + 5;
+        // extra crate of food increases ShipFood by 50%
+        ShipFood = ShipFood + ( ShipFood * 0.50);
+        System.out.println(ShipFood);
 
 
+        // 5 more babies join the ShipPopulation
+        ShipPopulation += 5;
+        System.out.println(ShipPopulation);
 
-        // Choose where to land: The Hill, The Plain, or The Ocean
         String LandingLocation = "The Ocean";
 
-        // Check to see if LandingLocation is equal to our desired location, ignoring case
-        if (LandingLocation.equalsIgnoreCase("The Ocean")) {
-            System.out.println("Bbzzz Landing on the Ocean");
+        // Check:
+        //  if the LandingLocation is equal to “The Plain” ignoring case
+        if (LandingLocation.equalsIgnoreCase("The Plain")) {
+            System.out.println("Bbzzz Landing on the Plain!!");
+        } else {
+            System.out.println("ERROR!!! Flight plan already set. Landing on the Plain.");
         }
-        else {
-            System.out.println("ERROR!!! Flight plan already set. Landing on the Ocean");
-        }
 
-        // Call landingCheck() that has a parameter of 100, returns a boolean, and set it equal to Landing
-        Landing = landingCheck(100);
+        // LandingCheck() that has a parameter of 100, returns boolean, and set it equal to Landing
+        Landing = LandingCheck(100);
 
-
-
-        // Mars Adventure Game Part 2
-        // Call GuessingGame (GuessingGame.java)
+        // call a new GuessingGame constructor
         new GuessingGame();
 
-
-
-        // Mars Adventure Game Part 3
-        // Call MarsExpedition (MarsExpedition.java)
+        // call a new MarsExpedition constructor
         new MarsExpedition();
 
-
-
-        // Mars Adventure Game Part 4
-        // Call FindingsLists (FindingsLists.java)
+        // call a new FindingsList constructor
         new FindingsLists();
     }
 
-
-    public static boolean landingCheck(int Loops) throws InterruptedException {
+    // Create a public static void function called LandingCheck with an int parameter called Loops
+    // Create a for loop, i = 0, loops until i is equal to Loops and inc by 1
+    // Check
+    // if i is divisible by 3 and divisible by 5, print "Keep Center"
+    // Else if i is divisible by 5, print "Right"
+    // Else if i is divisible by 3, print "Left"
+    // Otherwise print "Calculating"
+    // use Thread.sleep(250) after the if-else but prior to the end of the loop
+        // slows the console down by 250ms
+    // after the loop, print "Landed"
+    // return false
+    public static boolean LandingCheck(int Loops) throws InterruptedException {
         for (int i = 0; i <= Loops; i++) {
-            // Checks to see if i divisible by 3 equals 0, and that i divisible by 5 equals 0
             if (((i % 3) == 0) && ((i % 5) == 0)) {
                 System.out.println("Keep Center");
-            }
-            // Checks if i divisible by 5 equals 0
-            else if ((i % 5) == 0) {
+            } else if ((i % 5) == 0) {
                 System.out.println("Right");
-            }
-            // Checks if i divisible by 3 equals 0
-            else if ((i % 3) == 0) {
+            } else if ((i % 3) == 0) {
                 System.out.println("Left");
-            }
-            else {
+            } else {
                 System.out.println("Calculating");
             }
-
             Thread.sleep(250);
         }
-
         System.out.println("Landed");
 
-        // The ship landed on Mars! Return false value.
         return false;
     }
 }
